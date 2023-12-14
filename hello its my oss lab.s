6d64247f47e1d308c68e6560a1d8caf9537f31e6
jainik0745 <153814697+jainik0745@users.noreply.github.com>
2023-12-14 15:15:04 +0530

    Add files via upload

---
+++ hello its my oss lab.s
@@ -0,0 +1,15 @@
+<!DOCTTYPE.html>
+<html>
+<head>
+<title>Mypage<title>
+<!head>
+<body>
+<h1 style="color:white">Hello oss </h1>
+<h2 style="color:red">Hello oss </h2>
+<h3 style="color:orange">Hello oss </h3>
+<h4 style="color:lavender">Hello oss </h4>
+<h5 style="color:black">Hello oss </h5>
+<h6 style="color:purple">Hello oss </h6>
+<P>Hello its myossLab</p>
+</body>
+<html>
